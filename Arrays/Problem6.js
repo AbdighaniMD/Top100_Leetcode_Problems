4131d83279f0f3f47199f683e6f0b52572a0a913
AbdighaniMD <abdighanimd@outlook.com>
2023-09-05 22:25:26 +0100

    Problem6

--- Arrays/Problem6.js
+++ Arrays/Problem6.js
@@ -24,7 +24,34 @@ var trap = function(height) {
 };
 
 
+//--------------------------------------------
+
+var trap2 = function(height) {
+    
+    let start = new Array(height.length).fill(0);
+    let end = new Array(height.length).fill(0);
+    
+    start[0] = height[0];
+    end[height.length-1] = height[height.length-1];
+
+    for(let i = 1; i < height.length; i++){
+        start[i] = Math.max(start[i-1], height[i]);
+    }
+    
+    for(let i = height.length - 2; i >= 0; i--){
+        end[i] = Math.max(end[i+1], height[i]);
+    }
+
+    let sum = 0;
+    for(let i = 0; i < height.length; i++) {
+        sum += ((Math.min(start[i], end[i])) - height[i]);
+    }
+
+    return sum;
+};
+
+
 let height = [4,2,0,3,2,5]
 
-console.log(trap(height));
+console.log(trap2(height));
 //Output: 9
