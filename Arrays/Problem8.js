11037dce941d4cfceecdf0bed38c4d7467f64ef1
AbdighaniMD <abdighanimd@outlook.com>
2023-09-06 20:23:28 +0100

    Problem8

--- Arrays/Problem8.js
+++ Arrays/Problem8.js
@@ -1,10 +1,38 @@
 /**
- * 152 leetcode medium. Maximum Product Subarray
+ * 152 leetcode medium. Maximum Product Subarray [Kadane's Algorithm]
  * @param {number[]} nums
  * @return {number}
  */
 var maxProduct = function(nums) {
-    
+   
+    let sum = 0;
+    if (nums.length == 1) {
+        return nums[0];
+    }
+
+    let currentProduct = 1;
+    for(let i = 0; i < nums.length; i++) {
+      if (nums[i] != 0) {
+        currentProduct *= nums[i];
+        sum = Math.max(sum, currentProduct);
+      }
+      else {
+        currentProduct = 1;
+      }
+    }
+
+    currentProduct = 1;
+    for(let i = nums.length - 1; i >= 0; i--) {
+      if (nums[i] != 0) {
+        currentProduct *= nums[i];
+        sum = Math.max(sum, currentProduct);
+      }
+      else {
+        currentProduct = 1;
+      }
+    }
+  
+    return sum;
 };
 
 
