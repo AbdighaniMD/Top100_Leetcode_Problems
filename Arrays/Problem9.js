61b9c0ba75807f7a022db23e6959286df9b9a3d6
AbdighaniMD <abdighanimd@outlook.com>
2023-09-08 21:41:34 +0100

    Problem9

--- Arrays/Problem9.js
+++ Arrays/Problem9.js
@@ -5,8 +5,31 @@
  */
 var productExceptSelf = function(nums) {
     
+    let forwardArr = [];
+    let start = 1;
+
+    for(let i =0; i < nums.length; i++){
+        forwardArr.push(start);
+        start  = start * nums[i];
+    }
+   //console.log(forwardArr)
+
+    let res = [];
+    let start2 = 1;
+    for(let i = nums.length-1; i >= 0; i--){
+        res.unshift(start2*forwardArr[i]);
+        start2  = start2 * nums[i];
+    }
+
+    return res;
     
 };
+//-------------------------------------------------
+
+var productExceptSelf2 = function(nums) { 
+
+};
+
 
 let nums = [1,2,3,4] //Output: [24,12,8,6]
 console.log(productExceptSelf(nums))
