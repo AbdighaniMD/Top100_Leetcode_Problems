6dcbc38987bf49745fd926aaee8f0315951a563f
AbdighaniMD <abdighanimd@outlook.com>
2023-09-11 20:43:57 +0100

    Problem12

--- Arrays/Problem12.js
+++ Arrays/Problem12.js
@@ -5,20 +5,46 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotateNaive = function(nums, k) {
-    let left = 0;
-    let right =  nums.length-1;
-    for(let i = 0; i < nums.length; i++ ){
-              
-        if ( i < nums.length-k) {
-            let temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-            left++
-            right--;
-        }
 
+    // Step 1: Reverse the whole array
+    let start = 0;
+    let end = nums.length-1;
+    while( start < end){
+        let temp = nums[start];
+        nums[start]= nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
     }
+    //console.log(nums);
+
+    // Step 2: Reverse the first k elements
+    let start2 = 0;
+    let end2 = k-1;
+    for(let i = 0; i < k-1; i++ ){
+              
+            let temp = nums[start2];
+            nums[start2] = nums[end2];
+            nums[end2] = temp;
+            start2++
+            end2--;
+    }
+    //console.log(nums);
+    
+    // step 3: Reverse the remaining elements
+    let start3 = k;
+    let end3 = nums.length-1;
+    for(let i = 0; i < k-1; i++ ){
+              
+            let temp = nums[start3];
+            nums[start3] = nums[end3];
+            nums[end3] = temp;
+            start3++
+            end3--;
+    }
+
     return nums
+
 };
 
 //----------------------------------------------------------------
@@ -26,11 +52,13 @@ var rotateNaive = function(nums, k) {
 var rotate = function(nums, k) {
     k = k % nums.length;
 
-
-   //console.log(k);
+   console.log(k);
+   // Step 1: Reverse the whole array
    reverseArray(nums, 0, nums.length-1);
+   // Step 2: Reverse the first k elements
    reverseArray(nums, 0, k-1);
-   reverseArray(nums, k, nums.length-1 )
+   // step 3: Reverse the remaining elements
+   reverseArray(nums, k, nums.length-1)
 
 };
 
@@ -46,8 +74,8 @@ let reverseArray = function(nums, start, end){
 
 
 let nums = [1,2,3,4,5,6,7]
-let k = 3
+let k = 3 //Output: [5,6,7,1,2,3,4]
 
 console.log(rotateNaive(nums, k))
-//console.log(rotate(nums, k))
+console.log(rotate(nums, k))
 
