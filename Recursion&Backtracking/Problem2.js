0a4269b2cac4a1393fdeb799d8b1979a87ed2d54
AbdighaniMD <abdighanimd@outlook.com>
2023-09-19 17:29:27 +0100

    Problem2

--- Recursion&Backtracking/Problem2.js
+++ Recursion&Backtracking/Problem2.js
@@ -3,6 +3,7 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+
 var subsets = function(nums) {
     let result = [[]]
 
@@ -19,6 +20,35 @@ var subsets = function(nums) {
     return result
 };
 
+//------------------------------------------------
+let subsetsArr = [];
+let numsArr = [];
+
+var subsets2 = function(nums) {
+  let subset = [];
+  subsetsArr = [];
+  numsArr = nums;
+  backtrack(0, subset);
+  return subsetsArr;
+};
+
+function backtrack(i, subset) {
+  if (i >= numsArr.length) {
+    let subsetArr = [];
+    for(let i = 0; i < subset.length; i++) {
+      subsetArr.push(subset[i]);
+    }
+    subsetsArr.push(subsetArr);
+  }
+  else {
+    backtrack(i + 1, subset);
+
+    subset.push(numsArr[i]);
+    backtrack(i + 1, subset);
+    subset.pop();
+  }
+}
+
 
 let nums = [1,2,3]; //Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
-console.log(subsets(nums));
+console.log(subsets2(nums));
