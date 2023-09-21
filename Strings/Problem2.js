5e35e5f63380870e2c197093964846214a107cf4
AbdighaniMD <abdighanimd@outlook.com>
2023-09-21 13:44:49 +0100

    Problem2

--- Strings/Problem2.js
+++ Strings/Problem2.js
@@ -21,7 +21,27 @@ let groupAnagrams = function(strs) {
     return Object.values(object)
 }
 
+let groupAnagrams2 = function (strs) {
+    const map = new Map();
+
+    for (const str of strs) {
+
+        let key = str.split("").sort().join("");
+        
+        if (!map.has(key)) {
+            map.set(key, []);
+        }
+        map.get(key).push(str);
+    }
+
+    const result = [];
+    for (const key of map.keys()) {
+        result.push(map.get(key));
+    }
+    return result;
+}
 
 let strs = ["eat","tea","tan","ate","nat","bat"]  //Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 //console.log(strs[0].split("").sort().join(""))
 console.log(groupAnagrams(strs));
+console.log(groupAnagrams2(strs));
