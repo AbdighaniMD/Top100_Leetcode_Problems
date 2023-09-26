49df18d5a19382469ddbbc7f00123aec49061b59
AbdighaniMD <abdighanimd@outlook.com>
2023-09-26 19:44:44 +0100

    Problem7

---
+++ LinkedLists/Problem7.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * 876 leetcode easy. Middle of the Linked List
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let slow = head;
+    let fast = head;
+
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow
+};
