d1cd7e2353caacc979fbc583e6ae76998384c6fe
AbdighaniMD <abdighanimd@outlook.com>
2023-10-03 18:08:13 +0100

    Problem2

--- Stacks/Problem2.js
+++ Stacks/Problem2.js
@@ -1,43 +1,3 @@
-var MinStack = function() {
-    this.a = [];
-    this.b = [];
-};
-
-/** 
- * @param {number} val
- * @return {void}
- */
-MinStack.prototype.push = function(val) {
-    this.a.push(val);
-    if (this.b.length === 0 || this.b[this.b.length - 1] >= val) {
-        this.b.push(val);
-    }
-};
-
-/**
- * @return {void}
- */
-MinStack.prototype.pop = function() {
-    if (this.a[this.a.length - 1] === this.b[this.b.length - 1]) {
-        this.b.pop();
-    }
-    this.a.pop();
-};
-
-/**
- * @return {number}
- */
-MinStack.prototype.top = function() {
-    return this.a[this.a.length - 1];
-};
-
-/**
- * @return {number}
- */
-MinStack.prototype.getMin = function() {
-    return this.b[this.b.length - 1];
-};
-
 /** 
  * 155 leetcode Medium. Min Stack
  * Your MinStack object will be instantiated and called as such:
@@ -47,3 +7,81 @@ MinStack.prototype.getMin = function() {
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
  */
+
+
+var MinStack = function() {
+    this.stack = [];
+    this.minStack = [];
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+    if (this.minStack.length === 0 || this.minStack[this.minStack.length - 1] >= val) {
+        this.minStack.push(val);
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    /*if (this.stack[this.stack.length - 1] === this.minStack[this.minStack.length - 1]) {
+        this.minStack.pop();
+    }
+    this.stack.pop();*/
+    if (!this.stack.length) return null
+    this.minStack.pop();
+    return this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.minStack[this.minStack.length - 1];
+};
+
+//-------------------------------------------------------------------
+
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(val) {
+        if (!this.stack.length) {
+            this.stack.push(val);
+            this.minStack.push(val);
+        } else {
+            this.stack.push(val);
+            let lastValue = this.minStack[this.minStack.length - 1];
+            this.minStack.push(Math.min(lastValue, val));
+        }
+    }
+
+    pop() {
+        if (!this.stack.length) return null
+        this.minStack.pop();
+        return this.stack.pop();
+    }
+
+    top(){
+        return this.stack[this.stack.length -1]
+    }
+
+    getMin(){
+        return this.minStack[this.stack.length -1]
+    }
+}
