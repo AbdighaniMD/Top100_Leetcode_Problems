edabd6cfec761d2de760eec21905432ba5e5d17a
AbdighaniMD <abdighanimd@outlook.com>
2023-10-05 16:35:25 +0100

    Problem2

---
+++ Binary Search/Problem2.js
@@ -0,0 +1,66 @@
+/**
+ * 34 leetcode Medium. Find First and Last Position of Element in Sorted Array
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function binarySearch(nums, target) {
+    let find = [];
+    find[0] = findLowIndex(nums, target);
+    find[1] = findHighIndex(nums, target);
+    return find;
+}
+
+
+let findLowIndex = function (nums, target) {
+    //TODO: Write - Your - Code
+    let index = -1;
+
+    let start = 0;
+    let end = nums.length - 1;
+
+    while (start <= end) {
+
+        let mid = start + Math.floor((end - start) / 2);
+
+        if (nums[mid] >= target) {
+            end = mid - 1;
+        } else {
+            start = mid + 1
+        }
+
+        //find target 
+        if (nums[mid] === target) {
+            index = mid;
+        }
+    }
+    return index;
+};
+
+let findHighIndex = function (arr, target) {
+    //TODO: Write - Your - Code
+    let index = -1;
+    let left = 0;
+    let right = arr.length - 1;
+    while (left <= right) {
+
+        let mid = left + Math.floor((right - left) / 2);
+
+        if (arr[mid] <= target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1
+        }
+
+        //find target 
+        if (arr[mid] === target) {
+            index = mid;
+        }
+    }
+    return index;
+};
+
+
+let nums = [5,7,7,8,8,10]
+let target = 8 //Output: [3,4]
+console.log(searchRange(nums, target));
